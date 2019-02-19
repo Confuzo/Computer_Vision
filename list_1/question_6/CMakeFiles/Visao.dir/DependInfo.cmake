e0a188d9294c88e400259bfc8da1a2587b7a8648
Confuzo <abraaovld@gmail.com>
2019-02-19 19:16:44 -0300

    List 1 - Question 6

---
+++ list_1/question_6/CMakeFiles/Visao.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/confuzo/Área de Trabalho/disciplinas/visao_computacional/list_1/question_6/src/noiseCancel.cpp" "/home/confuzo/Área de Trabalho/disciplinas/visao_computacional/list_1/question_6/CMakeFiles/Visao.dir/src/noiseCancel.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/confuzo/Área de Trabalho/opencv-4.0.1/build"
+  "/home/confuzo/Área de Trabalho/opencv-4.0.1/include"
+  "/home/confuzo/Área de Trabalho/opencv-4.0.1/modules/core/include"
+  "/home/confuzo/Área de Trabalho/opencv-4.0.1/modules/flann/include"
+  "/home/confuzo/Área de Trabalho/opencv-4.0.1/modules/imgproc/include"
+  "/home/confuzo/Área de Trabalho/opencv-4.0.1/modules/ml/include"
+  "/home/confuzo/Área de Trabalho/opencv-4.0.1/modules/photo/include"
+  "/home/confuzo/Área de Trabalho/opencv-4.0.1/modules/dnn/include"
+  "/home/confuzo/Área de Trabalho/opencv-4.0.1/modules/gapi/include"
+  "/home/confuzo/Área de Trabalho/opencv-4.0.1/modules/imgcodecs/include"
+  "/home/confuzo/Área de Trabalho/opencv-4.0.1/modules/videoio/include"
+  "/home/confuzo/Área de Trabalho/opencv-4.0.1/modules/highgui/include"
+  "/home/confuzo/Área de Trabalho/opencv-4.0.1/modules/ts/include"
+  "/home/confuzo/Área de Trabalho/opencv-4.0.1/modules/features2d/include"
+  "/home/confuzo/Área de Trabalho/opencv-4.0.1/modules/calib3d/include"
+  "/home/confuzo/Área de Trabalho/opencv-4.0.1/modules/objdetect/include"
+  "/home/confuzo/Área de Trabalho/opencv-4.0.1/modules/stitching/include"
+  "/home/confuzo/Área de Trabalho/opencv-4.0.1/modules/video/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
