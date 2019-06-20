0b35b4d82e2d40acada0ff6051209b45d6b2df71
Confuzo <abraaovld@gmail.com>
2019-06-20 01:09:13 -0300

    drawing lines

--- homework_5/include/hough.hpp
+++ homework_5/include/hough.hpp
@@ -1,32 +1,79 @@
 #ifndef _HOUGH_
 #define _HOUGH_
 #include <opencv2/opencv.hpp>
+#include <vector>
 #include <string>
 
 using namespace cv;
 class Hough{
 
 public:
+    Mat img;
 
     Hough(std::string filename){
-        Mat img = imread(filename, 0);
-        straight(img);
+        img = imread(filename);
+        detectLines();
     }
 
 private:
 
-    void straight(Mat & img){
-        Mat src = img;
+    void detectLines(){
+        Mat src = img.clone();
+        cvtColor(src, src, cv::COLOR_RGB2GRAY);
         blur( src, src, Size(3,3) );
-        adaptiveThreshold(src, src, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY,13,4);
-        Canny(src, src, 127, 255, 3);
+        //adaptiveThreshold(src, src, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY,13,4);
+        Canny(src, src, 50, 150, 3);
+        imshow("teste1", src);
+        int diag = hypot(src.rows, src.cols);
+        int rho, theta;
+
+        std::vector<std::vector<int>> votes(2 * diag, std::vector<int>(180, 0));
         for(int x = 0; x < src.rows; x++){
             for(int y = 0; y < src.cols; y++){
+                if(src.at<uchar>(x,y) == 255){
+                    for(theta = 0; theta <= 180; theta++){
+                        int rho = round(x * cos(theta - 90) + y * sin(theta - 90)) + diag;
+                        votes[rho][theta]++;               
+                    }
+                }
+            }
+        }
+
+        drawLines(votes, diag);
+
+        imshow("teste", img);
+        waitKey();
+    }
+
+    void drawLines(std::vector<std::vector<int>> & votes, int const & diag){
+        
+        int rho, theta;
+        for(int i = 0; i < votes.size(); i++){
+            for(int k = 0; k < votes[i].size(); ++k){
+                if(votes[i][k] > 200){
+
+                    rho = i - diag;
+                    theta = k - 90;
+
+                    Point p1, p2;   
+                    
+                    int x0 = abs(cvRound(rho * cos(theta)));
+                    int y0 = abs(cvRound(rho * sin(theta)));
+                    
+                    p1.x = cvRound(x0 + 1000 * (-sin(theta)));
+	                p1.y = cvRound(y0 + 1000 * (cos(theta)));
+
+	                p2.x = cvRound(x0 - 1000 * (-sin(theta)));
+	                p2.y = cvRound(y0 - 1000 * (cos(theta)));
+
+                    line(img, p1, p2, Scalar(0, 0, 255));
+                }
                 
             }
         }
-        imshow("teste", src);
-        waitKey(0);
+        /*Point dummy1(10, 10), dummy2(100, 100);
+        line(img, dummy1, dummy2, Scalar(0, 0, 255), 1, LINE_AA);*/
+
     }
 };
 
