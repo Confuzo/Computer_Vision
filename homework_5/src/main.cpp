b2c8c53bbc26b09b3a3d3450e2f7e599472e147b
Confuzo <abraaovld@gmail.com>
2019-06-26 15:49:41 -0300

    updating main to receive parameters

--- homework_5/src/main.cpp
+++ homework_5/src/main.cpp
@@ -1,7 +1,13 @@
 #include "../include/hough.hpp"
+#include "../include/hough_circ.hpp"
 
 int main(int argc, char *argv[]){
     
-    Hough * h  = new Hough(argv[1], std::stoi(argv[2]));
+    int choice = std::stoi(argv[1]);
+    if(choice == 1){
+        Hough * h  = new Hough(argv[2], std::stoi(argv[3]));
+    } else {
+        Hough_circ * h = new Hough_circ(argv[2], std::stoi(argv[3]), std::stoi(argv[4]), std::stoi(argv[5]));
+    }
     return 0;
 }
